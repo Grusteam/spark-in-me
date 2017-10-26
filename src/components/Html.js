f3ae8d5fee1c8fed90df0c69d6d74a2b37c2f0b2
Serge <aka717@mail.ru>
2017-10-26 19:26:07 +0300

    styles restored

--- src/components/Html.js
+++ src/components/Html.js
@@ -78,6 +78,7 @@ class Html extends React.Component {
 					<link rel="stylesheet" href="/styles/font-awesome.css" />
 					<link rel="stylesheet" href="/styles/fa-viber.css" />
 					<link rel="stylesheet" href="/styles/clean-blog.css" />
+					<link rel="stylesheet" href="/highlight.css" /> 
 					
 					{styles.map(style =>
 						<style
